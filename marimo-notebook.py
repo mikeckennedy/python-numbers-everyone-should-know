998bad85c5d6eeee7e08050205ab5015ae79cf39
Michael Kennedy <mikeckennedy@gmail.com>
2025-12-27 17:57:15 -0800

    add marimo reporting

---
+++ marimo-notebook.py
@@ -0,0 +1,14 @@
+import marimo
+
+__generated_with = "0.18.4"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    # Just a test to start.
+    return
+
+
+if __name__ == "__main__":
+    app.run()
